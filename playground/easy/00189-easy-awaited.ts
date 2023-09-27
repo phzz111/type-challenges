b1ef2ddec03f83eda7f6d10a6b486fc4c8905913
piaohz <piaohz@ijiwei.com>
2023-09-27 16:55:12 +0800

    docs: awaited

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,7 +22,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends PromiseLike<infer P> ? P extends PromiseLike<any> ? Awaited<P> : P : never
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
