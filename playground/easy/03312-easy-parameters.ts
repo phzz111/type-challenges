73e769492c944084c29dc1329504dc3c0afc6a25
piaohz <piaohz@ijiwei.com>
2023-09-28 12:00:38 +0800

    docs: 完成简单难度的

--- playground/easy/03312-easy-parameters.ts
+++ playground/easy/03312-easy-parameters.ts
@@ -20,14 +20,14 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends ((...args: infer P) => any) ? P : never
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-const foo = (arg1: string, arg2: number): void => {}
-const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
-const baz = (): void => {}
+const foo = (arg1: string, arg2: number): void => { }
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => { }
+const baz = (): void => { }
 
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
