64ed4e19f921bccc2de52de9fb0ee8a1bb0438d7
piaohz <piaohz@ijiwei.com>
2023-11-30 15:15:15 +0800

    feat: 再试试

--- playground/easy/00004-easy-shi-xian-pick.ts
+++ playground/easy/00004-easy-shi-xian-pick.ts
@@ -34,7 +34,7 @@
 
 type MyPick<T, K> = {
   [P in keyof T as P extends K ? P : never]: T[P]
-} 
+}
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
