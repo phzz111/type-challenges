0f0a2b490fd8a13759e5811870ab90c5775c640b
piaohz <piaohz@ijiwei.com>
2023-11-30 14:55:07 +0800

    feat: 测试提交

--- playground/easy/00004-easy-shi-xian-pick.ts
+++ playground/easy/00004-easy-shi-xian-pick.ts
@@ -42,7 +42,6 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
   MyPick<Todo, 'title' | 'completed' | 'invalid'>,
 ]
 
